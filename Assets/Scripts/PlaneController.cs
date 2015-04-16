8e4166e7542d28ae544966fdffaf3e937fd519c6
sakataro <gorgo0013@gmail.com>
2015-04-16 22:50:01 +0900

    add plane controller

---
+++ Assets/Scripts/PlaneController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float dx = Input.GetAxis("Horizontal");
+		float dy = Input.GetAxis("Vertical");
+		transform.Translate (dx, dy, 0.0f);
+	}
+}
